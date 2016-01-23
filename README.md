a11f09ba38dd54539a6157e1c8cc5e2f00374887
Erik Poupaert <erik@sankuru.biz>
2016-01-23 10:58:17 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # base36x
-Implements a suggestion for base36 based on base64 encoding
+
+Implements a suggestion for base36 based on base64 encoding. You can find the suggestion for these functions [here](http://forums.phpfreaks.com/topic/218241-php-base36-encodingdecoding-for-textdata/).
+
+## Base36
+
+Base 36 is an encoding in which we only use letters (a to z) and digits (0 to 9) to represent data.
+
+It is quite similar to base 64, where we use lowercase letters (a to z), uppercase (A to Z) and digits (0 to 9) to represent the first 62 values, + and / to represent the two last values and = to correct the final translation block.
+
+## What is the problem?
+
+In fact, PHP supports converting to and from base36. However, only from base 10:
+
+	php -r "echo base_convert('555555555',10,36);"
+	96rher
+
+	php -r "echo base_convert('96rher',36,10);"
+	555555555
+
+Strings are not base 10. They are pretty much base 255. So, what we would need is:
+
+	php -r "echo base_convert('555555555',255,36);"
+
+	php -r "echo base_convert('96rher',36,255);"
+
+The standard PHP library does not implement this functionality. In fact, it would be quite possible to implement it based on existing functions, but the problem is that the standard algorithm needs support for large numbers in order to do that. Therefore, it would require installing the bcmath or gmp php extensions.
+
+## The workaround
+
+In fact, as the suggestion in the forum said, we can also repurpose the base64 encoding/decoding functions to produce base36 output. Now, the result is not a real base36 encoding, because not all combinations of valid base36 characters result in a valid base36 string. From there, the name `base36x` instead of `base36`.
+
+Note: But then again, this should not be an issue, because strings (base255) are usually not subjected to arithmetic operations such adding, substracting, multiyplying or dividing. The domain of strings and its operations are not expected to be a valid algebraic structure. Therefore, it does not particularly matter that the encoding domain is not entirely convex.
+
+You can use the encoding/decoding functions as following:
+
+	$base36x=base36x_encode($text);
+
+	$base255=base36x_decode($base36x);
+
+This solution does not require installing bcmath or gmp. Enjoy!
+
---
+++ base36x.php
@@ -0,0 +1,100 @@
+<?php
+
+/*	
+	==================================================================
+
+	base36x
+	Written By Erik Poupaert, Phnom Penh, Cambodia, Jan 2016
+	Implementing the idea in: 
+
+	http://forums.phpfreaks.com/topic/218241-php-base36-encodingdecoding-for-textdata/
+
+	==================================================================
+*/
+
+
+/* char_between: convenience macro */
+
+function char_between($char,$startCharRange,$endCharRange) {
+	return $char>=$startCharRange && $char<=$endCharRange; 
+}
+
+/* base36x_encode_char: encodes just one character */
+
+function base36x_encode_char($char) {
+	if(char_between($char,'a','z')) return $char;
+	if(char_between($char,'0','8')) return $char;
+	switch($char) {
+		case '9': return '90';
+		case '=': return '91'; 
+		case '/': return '92'; 
+		case '+': return '93';
+		default: return '9'.strtolower($char);
+	}
+}
+
+/* base36x_encode: encodes a string */
+
+function base36x_encode($string) {
+	$base64=base64_encode($string);
+	$result='';
+	for($i=0;$i<strlen($base64);$i++) {
+		$result.=base36x_encode_char($base64[$i]);
+	}
+	return $result;
+}
+
+/* base36x_decode_char: decodes 1 character, or 1 character and 1 lookahead */
+
+function base36x_decode_char($char,$lookahead) {
+	if(char_between($char,'a','z')) return $char;
+	if(char_between($char,'0','8')) return $char;
+	if($char=='9') {
+		switch($lookahead) {
+			case '0': return '9';
+			case '1': return '='; 
+			case '2': return '/'; 
+			case '3': return '+';
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9': return false; // 94-99 are not valid base36x sequences
+			default: return strtoupper($lookahead);
+		}
+	}
+	return false;
+}
+
+/* base36x_decode: decodes a string */
+
+function base36x_decode($string) {
+	if(!preg_match('|[a-z0-9]*|',$string)) {
+		//not a valid base36x string
+		return false;
+	}
+	$size=strlen($string);
+	$result='';
+	for($i=0;$i<$size-1;$i++) {
+		$transl=base36x_decode_char($string[$i],$string[$i+1]);
+		if($transl===false) {
+			return false; //sequence invalid
+		}
+		$result.=$transl;
+		if($string[$i]=='9') $i++; //lookahead consumed
+	}
+	return base64_decode($result);
+}
+
+//--------------------------------------
+// test
+//--------------------------------------
+
+$text="id=99&title=hello world";
+echo "text: $text\n";
+$base36x=base36x_encode($text);
+echo "base36x: $base36x\n";
+$base255=base36x_decode($base36x);
+echo "decoded: $base255\n";
+
